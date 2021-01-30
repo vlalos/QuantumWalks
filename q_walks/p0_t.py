c86737c6a7d78abe5265d2009e12a487b49fe7e4
Vlad Los <los.vladislav@gmail.com>
2021-01-30 21:06:02 +0100

    p0 count

--- q_walks/p0_t.py
+++ q_walks/p0_t.py
@@ -1,11 +1,42 @@
-from hadamard_1 import run
+import walkslib
 import matplotlib.pyplot as plt
-import numpy as np
 
-zeros = np.array([])
-for i in range(0, 10):
-    prob = run(i)
-    np.append(zeros, [prob[250]])
+ 
+init_state = [1/walkslib.sqrt2, 1/walkslib.sqrt2]
+step_number = 500
 
-print(zeros)
+zeros = [init_state]
+
+def run(steps, zero_prob):
+    # Initialise lists.
+    min, max = -500, 500
+    posn = [[0, 0] for i in range(min, max)]
+    posn[max] = init_state
     
+    
+    # Run for some steps...
+    for time in range(steps):
+        posn = walkslib.shift(walkslib.coin(posn))
+        zero_prob.append(posn[max])
+        
+    if (steps % 2 == 0):
+        zero_prob = zero_prob[0::2]
+        zeros_range = range(0, 2*len(zero_prob), 2)
+    else:
+        zero_prob = zero_prob[1::2]
+        zeros_range = range(1, 2*len(zero_prob), 2)
+    
+    zero_prob = walkslib.probabilities(zero_prob)
+
+    # Plot.
+    plt.plot(zeros_range, zero_prob)
+    plt.title(init_state)
+    plt.show()
+    
+    return zero_prob, zeros_range
+    
+
+"for i in range(1, 10):    run(i)"
+
+run(step_number, zeros)
+ 
